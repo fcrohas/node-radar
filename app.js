6cdbafa1aefcd4272b4f3ea20b16964264e9324c
Fabrice Crohas <fcrohas@gmail.com>
2015-02-15 19:37:53 +0100

    Add some other info to map

--- app.js
+++ app.js
@@ -6,7 +6,8 @@ var io = require('socket.io');
 var sbs1 = require('sbs1');
 
 // Connect to ads-b server
-var client = sbs1.createClient();
+var options = { host: '192.168.1.22', port:'30003'};
+var client = sbs1.createClient(options);
 
 // Set server port
 var server = app.listen(3000);
@@ -56,16 +57,31 @@ flight.on('connection', function(socket) {
 	  			socket.volatile.send(planes['ICAO'+msg.hex_ident]);	  			
 	  		}
 	  		if ((msg.ground_speed != null) && (current.ground_speed != msg.ground_speed)) {
-	  			planes['ICAO'+msg.hex_ident].ground_speed = msg.ground_speed * 1.8520; // km/h from knots
+	  			planes['ICAO'+msg.hex_ident].ground_speed = Math.floor(msg.ground_speed * 1.8520); // km/h from knots
+	  			socket.volatile.send(planes['ICAO'+msg.hex_ident]);	  			
+	  		}
+	  		if ((msg.vertical_rate != null) && (current.vertical_rate != msg.vertical_rate)) {
+	  			planes['ICAO'+msg.hex_ident].vertical_rate = Math.floor(msg.vertical_rate * 1.8520); // m/s from knots
+	  			socket.volatile.send(planes['ICAO'+msg.hex_ident]);	  			
+	  		}
+	  		if ((msg.squawk != null) && (current.squawk != msg.squawk)) {
+	  			planes['ICAO'+msg.hex_ident].squawk = msg.squawk;
 	  			socket.volatile.send(planes['ICAO'+msg.hex_ident]);	  			
 	  		}
 	  		if ((msg.altitude != null) && (current.altitude != msg.altitude)) {
-	  			planes['ICAO'+msg.hex_ident].altitude = msg.altitude * 0.3048; // feet en m
+	  			planes['ICAO'+msg.hex_ident].altitude = Math.floor(msg.altitude * 0.3048); // feet en m
+	  			socket.volatile.send(planes['ICAO'+msg.hex_ident]);
+	  		}
+	  		if ((msg.logged_time != null) && (current.logged_time != msg.logged_time)) {
+	  			var msgTime = msg.logged_timestamp();
+	  			//console.log(((msgTime - planes['ICAO'+msg.hex_ident].live_time) / 1000));
+  				planes['ICAO'+msg.hex_ident].quality = ((msgTime - planes['ICAO'+msg.hex_ident].live_time) / 1000) < 4; // 30 s delay bad quality
+	  			planes['ICAO'+msg.hex_ident].live_time = msgTime;
 	  			socket.volatile.send(planes['ICAO'+msg.hex_ident]);
 	  		}
 		}
 		else {
-			planes['ICAO'+msg.hex_ident] = {'ICAO':msg.hex_ident,'latitude' : msg.lat, 'longitude' : msg.lon, 'title' : msg.ICAO, 'track': 90, 'callsign' : 'unknown', 'ground_speed':0, 'altitude' : 0};
+			planes['ICAO'+msg.hex_ident] = {'ICAO':msg.hex_ident,'latitude' : msg.lat, 'longitude' : msg.lon, 'title' : msg.ICAO, 'track': 90, 'callsign' : 'unknown', 'ground_speed':0, 'altitude' : 0, 'vertical_rate':0, 'squawk' : 0, 'out_of_bound' : false, 'live_time': msg.logged_timestamp(), 'quality':true};
 			socket.volatile.send(planes['ICAO'+msg.hex_ident]);			
 		}
 	  }
