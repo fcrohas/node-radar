3cd5812c5ed1294e4a3918ab2e665d530f6db0ee
Fabrice Crohas <fcrohas@gmail.com>
2015-05-02 23:16:28 +0200

    Another step refactory track

--- public/js/controllers/MainCtrl.js
+++ public/js/controllers/MainCtrl.js
@@ -1,35 +1,5 @@
 angular.module('controllers').controller('mainCtrl', ['$scope', 'SocketService', function ($scope,socket) {
-  var selectedPlane = '';
 
-  $scope.connected = false;
   $scope.socket = socket;
-
-  // Register general selected action
-  $scope.$on('planeSelect', function(event, ICAO) { 
-    // look for plane id to unselect
-    if (selectedPlane != '') {
-      $scope.$broadcast('planeUnSelected',selectedPlane);  
-    }
-    $scope.$broadcast('planeSelected',ICAO);
-    selectedPlane = ICAO;    
-  });  
-
-  $scope.$on('planeUnSelect', function(event, ICAO) { 
-    $scope.$broadcast('planeUnSelected',ICAO);  
-    selectedPlane = '';
-  });
-
-  // Connect only once
-  if ($scope.connected == false) {
-    // Detect connection
-    socket.on('connect', function() {
-      $scope.connected = true;
-    });
-
-    // Detect dicconnect
-    socket.on('disconnect', function() {
-      $scope.connected = false;
-    });
-  }
   
 }]);
--- public/js/services/PlaneService.js
+++ public/js/services/PlaneService.js
@@ -7,12 +7,36 @@ angular.module('services').factory('PlaneService', ['$http', function($http) {
   }
 
   return {
-    getInfo: function(params, callback) {
+      // Make gradient color between two color
+    makeGradientColor : function(color1, color2, percent) {
+      var newColor = {};
+      function makeChannel(a, b) {
+          return(a + Math.round((b-a)*(percent/100)));
+      }
+      function makeColorPiece(num) {
+          num = Math.min(num, 255);   // not more than 255
+          num = Math.max(num, 0);     // not less than 0
+          var str = num.toString(16);
+          if (str.length < 2) {
+              str = "0" + str;
+          }
+          return(str);
+      }
+      newColor.r = makeChannel(color1.r, color2.r);
+      newColor.g = makeChannel(color1.g, color2.g);
+      newColor.b = makeChannel(color1.b, color2.b);
+      newColor.cssColor = "#" + 
+                          makeColorPiece(newColor.r) + 
+                          makeColorPiece(newColor.g) + 
+                          makeColorPiece(newColor.b);
+      return(newColor);
+    },
+    getInfo: function(params) {
       return $http.get('/rest/aircraft/info/'+ params).then(function(response) {
         return response.data;   
       });
     },
-    getTrackHistory: function(params, callback) {
+    getTrackHistory: function(params) {
       return $http.get('/rest/aircraft/history/'+ params).then(function(response) {
         return response.data;
       });
@@ -22,7 +46,6 @@ angular.module('services').factory('PlaneService', ['$http', function($http) {
       startLong = radians(startLong);
       endLat = radians(endLat);
       endLong = radians(endLong);
-
       var dLong = endLong - startLong;
 
       var dPhi = Math.log(Math.tan(endLat/2.0+Math.PI/4.0)/Math.tan(startLat/2.0+Math.PI/4.0));
@@ -32,7 +55,6 @@ angular.module('services').factory('PlaneService', ['$http', function($http) {
         else
            dLong = (2.0 * Math.PI + dLong);
       }
-
       return (degrees(Math.atan2(dLong, dPhi)) + 360.0) % 360.0;
     }
   };
